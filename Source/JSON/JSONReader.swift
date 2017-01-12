6eb292f57262dca34db79e147fc0de6fbe5c807e
Andreas Thenn <andreas.thenn@runtastic.com>
2017-01-12 09:39:20 +0100

    ADD JSONReader to read a json file into a dictionary (JSONObject) MOD changed SwiftLint line length warning to 150

---
+++ Source/JSON/JSONReader.swift
@@ -0,0 +1,39 @@
+//
+//  JsonReader.swift
+//  Matrioska
+//
+//  Created by Andreas Thenn on 12/01/2017.
+//  Copyright © 2017 runtastic. All rights reserved.
+//
+
+import Foundation
+
+public typealias JSONObject = [String: AnyObject]
+
+public class JSONReader {
+
+    class func getJSON(from data: Data) -> JSONObject? {
+        do {
+            let json = try JSONSerialization.jsonObject(with: data,
+                                                        options: JSONSerialization.ReadingOptions()) as? JSONObject
+            return json
+        } catch {
+            assert(false, "JSON-File couldn't be parsed")
+            return nil
+        }
+    }
+
+    public class func jsonObject(from jsonFilename: String, bundle: Bundle = Bundle.main) -> JSONObject? {
+        guard let filePath = bundle.path(forResource: jsonFilename, ofType: "json") else {
+            return nil
+        }
+        do {
+            let data = try NSData(contentsOfFile: filePath,
+                                  options: NSData.ReadingOptions.uncached)
+            return getJSON(from: data as Data)
+        } catch {
+            assert(false, "JSON-File couldn't be read")
+            return nil
+        }
+    }
+}
