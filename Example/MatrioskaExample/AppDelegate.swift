d218084c1d79f51fa227295d90e6a59fee3e68dd
Joan Romano <joan.romano@runtastic.com>
2017-01-13 21:32:22 +0100

    Fix code in example

--- Example/MatrioskaExample/AppDelegate.swift
+++ Example/MatrioskaExample/AppDelegate.swift
@@ -54,7 +54,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
-        let _ = JSONFactory()
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.rootViewController = rootComponent.viewController()
         window?.makeKeyAndVisible()
