7c428d5086d3e003029005ecc5ecae6cd8355e0b
Andreas Thenn <andreas.thenn@runtastic.com>
2017-03-08 16:59:11 +0100

    ADD support for StackViewController to scroll into a child view controller

--- Source/Views/StackViewController.swift
+++ Source/Views/StackViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 import SnapKit
 
+public enum RelativePosition {
+    case topOrLeft, center, bottomOrRight
+}
+
 /// A ViewController that contains a stackView
 final class StackViewController: UIViewController {
     
@@ -24,7 +28,7 @@ final class StackViewController: UIViewController {
         self.preserveParentWidth = configuration.preserveParentWidth
         
         super.init(nibName: nil, bundle: nil)
-
+        
         title = configuration.title
         stackView.distribution = .equalSpacing
         stackView.axis = configuration.axis
@@ -33,11 +37,11 @@ final class StackViewController: UIViewController {
         if !preserveParentWidth {
             stackView.alignment = .leading
         }
-
+        
         scrollView.backgroundColor = configuration.backgroundColor
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    required init?(coder _: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
@@ -48,13 +52,13 @@ final class StackViewController: UIViewController {
         scrollView.alwaysBounceVertical = false
         scrollView.showsHorizontalScrollIndicator = false
         
-        scrollView.snp.makeConstraints { (make) in
+        scrollView.snp.makeConstraints { make in
             make.edges.equalTo(view)
         }
         
         scrollView.addSubview(stackView)
         
-        stackView.snp.makeConstraints { (make) in
+        stackView.snp.makeConstraints { make in
             make.edges.equalTo(scrollView)
             if preserveParentWidth {
                 make.width.equalTo(view)
@@ -68,5 +72,51 @@ final class StackViewController: UIViewController {
         addChildViewController(childViewController)
         stackView.addArrangedSubview(childViewController.view)
         childViewController.didMove(toParentViewController: self)
+    }
+    
+    public func scrollToViewController(target targetViewController: UIViewController,
+                                       position relativePosition: RelativePosition = .center) {
+        
+        guard self.childViewControllers.contains(targetViewController) else {
+            return
+        }
+        
+        var targetOffset = CGPoint(x: 0, y: 0)
+        let targetFrame = targetViewController.view.frame
+        
+        if stackView.axis == .vertical {
+            targetOffset.x = -scrollView.contentInset.left
+            let topOffset = targetFrame.origin.y - scrollView.contentInset.top
+            let bottomOffset = targetFrame.origin.y + targetFrame.height - scrollView.bounds.height
+            
+            switch relativePosition {
+            case .topOrLeft:
+                targetOffset.y = topOffset
+                break
+            case .center:
+                targetOffset.y = topOffset - bottomOffset - (targetFrame.height / 2)
+                break
+            case .bottomOrRight:
+                targetOffset.y = bottomOffset
+                break
+            }
+        } else {
+            targetOffset.y = -scrollView.contentInset.top
+            let rightOffset = targetFrame.origin.x + targetFrame.width - scrollView.bounds.width
+            
+            switch relativePosition {
+            case .topOrLeft:
+                targetOffset.x = targetFrame.origin.x - scrollView.contentInset.left
+                break
+            case .center:
+                targetOffset.x = -(targetOffset.x - rightOffset - (targetFrame.width / 2))
+                break
+            case .bottomOrRight:
+                targetOffset.x = rightOffset
+                break
+            }
+        }
+        
+        scrollView.setContentOffset(targetOffset, animated: true)
     }
 }
